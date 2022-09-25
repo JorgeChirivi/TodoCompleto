5ab696fa9a7a10cfd7e49b22e13c86068f270daa
Jorge Chirivi <43974887+JorgeChirivi@users.noreply.github.com>
2022-09-25 14:13:01 -0500

    actualizando archivo

--- src/main/java/com/ciclo3/TodoCompleto/Repositories/repositoryEnteprice.java
+++
@@ -1,7 +0,0 @@
-package com.ciclo3.TodoCompleto.Repositories;
-
-import com.ciclo3.TodoCompleto.Model.Enterprice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repositoryEnteprice extends JpaRepository<Enterprice,Long> {
-}
